5ee44c8241e0c54c6f8ccea652ef9c5bbdd332a1
lisvv <lisyansky1234@yandex.ru>
2021-10-08 22:13:50 +0300

    add r expression

--- part1/director/test.py
+++ part1/director/test.py
@@ -12,14 +12,14 @@ class DirectorsTestCase(SkyproTestCase):
     def test_query_structure_has_distinct_method(self):
         keywords = self.student_query.get('query_info').get('keywords')
         self.assertIn('distinct', keywords,
-                      ('%@Проверьте, что в результате запроса'
+                      (r'%@Проверьте, что в результате запроса'
                        'не повторяются имена режисcеров'))
 
     def test_query_columns_is_correct(self):
         student_columns = self.student_query.get('cursor_info').get('columns')
         author_columns = self.author_query.get('cursor_info').get('columns')
         self.assertEqual(student_columns, author_columns,
-                         ('%@Проверьте, что правильно выбрали'
+                         (r'%@Проверьте, что правильно выбрали'
                           ' колонку в базе данных. '
                           f'Вы выбрали {student_columns}, тогда '
                           f'как необходимо {author_columns}'))
@@ -28,7 +28,7 @@ class DirectorsTestCase(SkyproTestCase):
         count = self.student_query.get('cursor_info').get('rows_count')
         author_count = self.author_query.get('cursor_info').get('rows_count')
         self.assertFalse(count > author_count,
-                         ('%@Кажется, в запросе имеется лишнее условие.'
+                         (r'%@Кажется, в запросе имеется лишнее условие.'
                           f'Выводится больше строк ({count}) '
                           f'чем предполагалось {author_count}'))
 
@@ -36,7 +36,7 @@ class DirectorsTestCase(SkyproTestCase):
         count = self.student_query.get('cursor_info').get('rows_count')
         author_count = self.author_query.get('cursor_info').get('rows_count')
         self.assertFalse(count < author_count,
-                         ('%@Кажется, в запросе нехватает условия.'
+                         (r'%@Кажется, в запросе нехватает условия.'
                           f'Выводится меньше строк ({count}) '
                           f'чем предполагалось {author_count}'))
 
